33e5d0962dbca3a87eb2327deb7101537498b664
michealhansun <hansun@live.com>
2021-04-19 00:38:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,13 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/cokebar/luci-app-vlmcsd.git package/luci-app-vlmcsd
+git clone https://github.com/cokebar/openwrt-vlmcsd.git package/openwrt-vlmcsd
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git package/openwrt-smartdns
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/project-lede/luci-app-godproxy.git package/luci-app-godproxy
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
