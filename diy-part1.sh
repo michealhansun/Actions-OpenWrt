83679670bc35370d9d1fa610f71c1bea65aec8d2
michealhansun <hansun@live.com>
2023-04-06 20:40:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kuoruan https://github.com/kuoruan/openwrt-frp' >>feeds.conf.default
